4b847afea63f7e0c9f67a00df7cca382efbafbe0
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2022-09-08 10:14:30 +0200

    Added panic handler

--- panic.go
+++ panic.go
@@ -1,6 +1,24 @@
 package panic
 
+type PanicHandler func(interface{}) bool
+
+var PanicHandlers = []PanicHandler{}
+
+func SetPanicHandler(handler PanicHandler) {
+	PanicHandlers = append(PanicHandlers, handler)
+}
+
+func HandleCrash() {
+	if r := recover(); r != nil {
+		for _, handler := range PanicHandlers {
+			handler(r)
+		}
+		panic(r)
+	}
+}
+
 func Panic(n int) {
+	defer HandleCrash()
 	if n == 10 {
 		panic("I'm panicing")
 	}
